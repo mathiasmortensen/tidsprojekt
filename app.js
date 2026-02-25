8ff09b60c624b7562d2e43a3f2bc0602e0db82f2
Mathias Mortensen <mamo0010@stud.ek.dk>
2026-02-25 11:17:47 +0100

    ændret fra CET til UTC

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(8080, (error) => {
